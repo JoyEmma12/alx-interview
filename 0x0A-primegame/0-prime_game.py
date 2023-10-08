aacb0f01a6a1ffef6feece3ee26658a8fb27f382
JoyEmma12 <ejoy0600@gmail.com>
2023-10-09 00:34:08 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,34 +3,44 @@
 Game of choosing Prime numbers froma set
 """
 
-
-def primeNumbers(n):
-    """Return list of prime numbers between 1 and n inclusive
-       Args:
-        n (int): upper boundary of range. lower boundary is always 1
-    """
-    primeNos = []
-    filtered = [True] * (n + 1)
-    for prime in range(2, n + 1):
-        if (filtered[prime]):
-            primeNos.append(prime)
-            for i in range(prime, n + 1, prime):
-                filtered[i] = False
-    return primeNos
-
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n+1)
+    primes[0] = primes[1] = False
+    p = 2
+    while p*p <= n:
+        if primes[p]:
+            for i in range(p*p, n+1, p):
+                primes[i] = False
+        p += 1
+    return [i for i in range(n+1) if primes[i]]
 
 def isWinner(x, nums):
-    if x is None or nums is None or x == 0 or nums == []:
-        return None
-    Maria = Ben = 0
-    for i in range(x):
-        primeNos = primeNumbers(nums[i])
-        if len(primeNos) % 2 == 0:
-            Ben += 1
+    def can_win(n, primes):
+        dp = [False] * (n+1)
+        dp[0] = False
+        dp[1] = False
+
+        for i in range(2, n+1):
+            if i in primes:
+                dp[i] = not dp[i-1]
+
+        return dp[n]
+
+    winners = {'Maria': 0, 'Ben': 0}
+
+    for n in nums:
+        if n == 1:
+            winners['Ben'] += 1
         else:
-            Maria += 1
-    if Maria > Ben:
-        return 'Maria'
-    elif Ben > Maria:
-        return 'Ben'
-    return None
+            primes = sieve_of_eratosthenes(n)
+            if can_win(n, primes):
+                winners['Maria'] += 1
+            else:
+                winners['Ben'] += 1
+
+    max_wins = max(winners.values())
+
+    if list(winners.values()).count(max_wins) == 1:
+        return max(winners, key=winners.get)
+    else:
+        return None
